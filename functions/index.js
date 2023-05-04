a46e757701240f31b38845bc55fa9310454e7a5e
moshoodmohammed <mohammedamoshood@gmail.com>
2023-05-05 00:45:34 +0100

    fixed up some stuff

--- functions/index.js
+++ functions/index.js
@@ -7,38 +7,38 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-const functions = require('firebase-functions');
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const functions = require("firebase-functions");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const blogRoutes = require('../Routes/blogRoutes');
+const blogRoutes = require("../Routes/blogRoutes");
 
 // Mongodb connection
-const dbUrl = 'mongodb+srv://moshoodmohammed:managermuhkid@cluster1.e4kvies.mongodb.net/node-tuit?retryWrites=true&w=majority';
+const dbUrl = "mongodb+srv://moshoodmohammed:managermuhkid@cluster1.e4kvies.mongodb.net/node-tuit?retryWrites=true&w=majority";
 mongoose.connect(dbUrl)
     .then((result) => {
-        console.log('connected to db');
+      console.log("connected to db");
     })
     .catch((err) => console.log(err));
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-app.use(morgan('dev'));
-app.use('/blogs',blogRoutes);
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use("/blogs", blogRoutes);
 
-app.get('/',(req,res) =>{
-    res.redirect('/blogs');
+app.get("/", (req, res) =>{
+  res.redirect("/blogs");
 });
 
-app.get('/about',(req,res) =>{
-    res.render('about',{title: 'About'});
+app.get("/about", (req, res) =>{
+  res.render("about", {title: "About"});
 });
 
-app.use((req,res) =>{
-    res.status(404).render('404',{title: '404'});
+app.use((req, res) =>{
+  res.status(404).render("404", {title: "404"});
 });
 
 exports.app = functions.https.onRequest(app);
