95f2ea41bd324f7a828ca04ddaf816cbe5a1d5f3
moshoodmohammed <mohammedamoshood@gmail.com>
2023-05-04 03:23:26 +0100

    firebase sdk setup

---
+++ views/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZy9Ia39GUe92hSNwMQS5sltMakXMsMqw",
+  authDomain: "first-node-project-20e01.firebaseapp.com",
+  projectId: "first-node-project-20e01",
+  storageBucket: "first-node-project-20e01.appspot.com",
+  messagingSenderId: "390669648016",
+  appId: "1:390669648016:web:ac210b8ceacc38bd189815"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
